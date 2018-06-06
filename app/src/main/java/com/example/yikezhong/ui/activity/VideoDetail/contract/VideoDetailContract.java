6007299aa6ed7eeba7399c579854a9208b49a9ab
guizhenwg <wg15201369698@163.com>
2018-06-06 22:43:46 +0800

    更新视频详情

---
+++ app/src/main/java/com/example/yikezhong/ui/activity/VideoDetail/contract/VideoDetailContract.java
@@ -0,0 +1,18 @@
+package com.example.yikezhong.ui.activity.VideoDetail.contract;
+
+import com.example.yikezhong.bean.VideoDetailBean;
+import com.example.yikezhong.ui.base.BaseContract;
+
+/**
+ * Created by lenovo on 2018/6/6.
+ */
+
+public interface VideoDetailContract {
+    interface View extends BaseContract.BaseView {
+        void getVideoDetailSuccess(VideoDetailBean Bean);
+    }
+
+    interface Presenter extends BaseContract.BasePresenter<View> {
+        void getVideoDetail(String token,String wid);
+    }
+}
---
+++ app/src/main/java/com/example/yikezhong/ui/activity/VideoDetail/presenter/VideoDetailPresenter.java
@@ -0,0 +1,56 @@
+package com.example.yikezhong.ui.activity.VideoDetail.presenter;
+
+import com.example.yikezhong.bean.VideoDetailBean;
+import com.example.yikezhong.net.VideoDetailApi;
+import com.example.yikezhong.ui.activity.VideoDetail.contract.VideoDetailContract;
+import com.example.yikezhong.ui.base.BasePresenter;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by lenovo on 2018/6/6.
+ */
+
+public class VideoDetailPresenter extends BasePresenter<VideoDetailContract.View> implements VideoDetailContract.Presenter{
+    private VideoDetailApi videoDetailApi;
+
+    @Inject
+    public VideoDetailPresenter(VideoDetailApi videoDetailApi) {
+        this.videoDetailApi = videoDetailApi;
+    }
+
+    @Override
+    public void getVideoDetail(String token, String wid) {
+        videoDetailApi.getReMen(token,wid)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<VideoDetailBean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(VideoDetailBean videoDetailBean) {
+                        if (mView!=null){
+                            mView.getVideoDetailSuccess(videoDetailBean);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+}
