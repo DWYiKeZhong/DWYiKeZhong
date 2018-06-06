f57917383606a34c2a2818500a243d2dae58ce74
guizhenwg <wg15201369698@163.com>
2018-06-06 09:53:25 +0800

    视频更新

--- app/src/main/java/com/example/yikezhong/net/HotVideoApiService.java
+++ app/src/main/java/com/example/yikezhong/net/HotVideoApiService.java
@@ -3,14 +3,16 @@ package com.example.yikezhong.net;
 import com.example.yikezhong.bean.HotVideoBean;
 
 import io.reactivex.Observable;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by lenovo on 2018/6/5.
  */
 
 public interface HotVideoApiService {
-    @GET("quarter/getHotVideos?token=100&source=android&appVersion=101")
+    @GET("quarter/getHotVideos?token=100")
     Observable<HotVideoBean> GetHotVideo();
 
 }
